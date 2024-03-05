20fafd98d66ecdd772211df0e901807b7dcd36ea
amittad <144377558+amittad@users.noreply.github.com>
2024-03-05 18:24:34 +0530

    Add files via upload

---
+++ day2/day2_challang2.py
@@ -0,0 +1,7 @@
+# default argument and parameter
+
+def add(a, b, c, d=20):  # default parameter d=20 , overtake the argument 44 take value 20
+    print(a+b+c+d)
+
+
+add(11, 22, 33, 44)
---
+++ day2/day2_challange1.py
@@ -0,0 +1,14 @@
+# function programm to check even or odd number
+def number(num):
+    if num % 2 == 0:
+        print(f"{num} = is even")
+    else:
+        print(f"{num} = is odd")
+
+
+number(22)
+number(21)
+number(27.4)
+number(27)
+number(20)
+
