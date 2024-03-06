e8faf40cba61a49e4d6832f6dd97a0a971d4449e
amittad <144377558+amittad@users.noreply.github.com>
2024-03-06 20:32:46 +0530

    Add files via upload

---
+++ day3/day3.py
@@ -0,0 +1,36 @@
+# string
+# question 1:-indexing of string
+
+# a = "amit tad"
+# # print(a[0], a[2], a[5])
+# # print(a[-1], a[-6])   # negative indexing start from last letter in array
+
+
+# 2] string slicing
+#
+# a = "hello linkdin connection"
+# print(a[0:9:1]) # slice the string in index 0 to index 9 and print after slicing 1 position
+# print(a[1:10:])
+# print(a[-1:10:-1]) # slice the reverse string
+
+
+# 3] string len
+
+# a = "amit tad the coder"
+# print(len(a)) # print the length of a. a is a variable store the this string
+#
+
+# ################################################################################################################################
+
+################  4 ] 2 way of iterating string ##################
+# Q 1]
+# a = " amit gangadhar tad"
+#
+# for var in range(0,len(a)):
+#     print(a[var]) # print string using loop in range function
+
+# Q 2 ]
+# a = " amit gangadhar tad"
+#
+# for var in a:
+#     print(var) # print string using loop
